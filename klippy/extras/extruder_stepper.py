78f9e4f9de3270f639b09700351da19529cccea0
naikymen <you@example.com>
2022-12-20 15:03:48 -0300

    understanding extruder_stepper and PrinterExtruderStepper

--- klippy/extras/extruder_stepper.py
+++ klippy/extras/extruder_stepper.py
@@ -6,10 +6,20 @@
 import logging
 from kinematics import extruder
 
+# NOTE: this class is loaded when an "extruder_stepper" section is defined in a config file.
+#       See: https://www.klipper3d.org/Config_Reference.html?h=mcu#extruder_stepper
+
 class PrinterExtruderStepper:
     def __init__(self, config):
         self.printer = config.get_printer()
+        
+        # NOTE: defines an extruder stepper from the config section.
+        #       During initialization of the "ExtruderStepper" class,
+        #       a stepper is created using the same "PrinterStepper"
+        #       function used by "PrinterRail" (in stepper.py).
+        #
         self.extruder_stepper = extruder.ExtruderStepper(config)
+        
         self.extruder_name = config.get('extruder')
         self.printer.register_event_handler("klippy:connect",
                                             self.handle_connect)
