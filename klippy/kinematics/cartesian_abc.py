ee5de766723f03121cbf2134d716358ccca919b0
naikymen <you@example.com>
2023-02-27 18:47:50 -0300

    fix

--- klippy/kinematics/cartesian_abc.py
+++ klippy/kinematics/cartesian_abc.py
@@ -5,6 +5,7 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging
 import stepper
+from kinematics import CartKinematics
 
 class CartKinematicsABC(CartKinematics):
     """Kinematics for the extra toolhead class.
