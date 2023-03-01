e54c8bab8f4a0f6944b72073273e25de9d8c2f84
naikymen <you@example.com>
2023-03-01 16:58:31 -0300

    fix

--- klippy/kinematics/cartesian_abc.py
+++ klippy/kinematics/cartesian_abc.py
@@ -149,7 +149,7 @@ class CartKinematicsABC(CartKinematics):
         Has no effect on XYZ IDs
         """
         xyz_ids = [0, 1, 2, 0, 1, 2]
-        if isinstance(ini_list1, list):
+        if isinstance(axes, list):
             return [xyz_ids[i] for i in axes]
         else:
             return xyz_ids[axes]
